e4ea6f2b89b4b9dbfd2e3ffb16ba4e0c4b67a9dc
Debugg3R <debugg3r@gmail.com>
2018-10-24 22:30:23 +0300

    First interface
    First basic interface

--- app/src/main/java/com/debugg3r/android/testdecisions/MainActivity.kt
+++ app/src/main/java/com/debugg3r/android/testdecisions/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.debugg3r.android.testdecisions
 
+import android.content.Intent
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val LOG_TAG = this::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_questions.setOnClickListener { view -> questionsOnClick(view)}
+    }
+
+    fun questionsOnClick(view: View) {
+        var intent = Intent(this, QuestionActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/debugg3r/android/testdecisions/QuestionActivity.kt
@@ -0,0 +1,13 @@
+package com.debugg3r.android.testdecisions
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+
+    }
+}
