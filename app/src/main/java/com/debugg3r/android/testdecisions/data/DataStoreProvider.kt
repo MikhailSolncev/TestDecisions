9a40006baf01e0069791be38d0a80ceaedc3bded
Debugg3R <debugg3r@gmail.com>
2018-11-05 18:57:23 +0300

    Questions turned editable
    Questions in list turned tu editable

--- app/src/main/java/com/debugg3r/android/testdecisions/data/DataStoreProvider.kt
+++ app/src/main/java/com/debugg3r/android/testdecisions/data/DataStoreProvider.kt
@@ -5,63 +5,72 @@ import kotlin.IndexOutOfBoundsException
 import kotlin.collections.HashMap
 
 open class DataStoreProvider() : DataStore  {
-    private val questionList = HashMap<String, Question>()
+    private val questionMap = HashMap<String, Question>()
+    private val questionList = LinkedList<Question>()
 
     private object Holder {val instance = DataStoreProvider() }
 
     companion object {
-
         val instance: DataStore by lazy { Holder.instance }
     }
 
     override fun getQuestions(): Collection<Question> {
-        return questionList.values //as Collection<Question>
+        return questionMap.values //as Collection<Question>
     }
 
     override fun getQuestion(uid: String): Question {
-        if (!questionList.containsKey(uid))
+        if (!questionMap.containsKey(uid))
             throw IndexOutOfBoundsException("Can't find question by uid '{$uid}'")
-        return questionList.get(uid)!!
+        return questionMap.get(uid)!!
     }
 
     override fun getQuestion(position: Int): Question {
-        if (position >= questionList.count())
+        if (position >= questionList.size)
             throw IndexOutOfBoundsException("Can't find question on position '{$position}'")
-        return questionList.values.elementAt(position)
+        return questionList.get(position)
     }
 
     override fun addQuestion(question: Question): String {
-        if (!findQuestion(question))
-            questionList.put(question.uid, question)
+        if (!findQuestion(question)) {
+            questionMap.put(question.uid, question)
+            questionList.add(question)
+        }
         return question.uid
     }
 
     override fun addQuestion(text: String): Question {
         val question = Question(text)
-        questionList.put(question.uid, question)
+        questionMap.put(question.uid, question)
+        questionList.add(question)
         return question
     }
 
     override fun findQuestion(question: Question): Boolean {
-        return questionList.containsKey(question.uid)
+        return questionMap.containsKey(question.uid)
     }
 
     override fun findQuestion(uid: String): Boolean {
-        return questionList.containsKey(uid)
+        return questionMap.containsKey(uid)
     }
 
     override fun getCount(): Int {
-        return questionList.count()
+        return questionList.size
     }
 
     override fun removeQuestion(question: Question) {
-        if (!questionList.containsKey(question.uid))
+        if (!questionMap.containsKey(question.uid))
             throw IndexOutOfBoundsException("Can't find question by uid '{$question.uid}'")
-        questionList.remove(question.uid)
+        questionMap.remove(question.uid)
+        questionList.remove(question)
     }
 
     override fun removeQuestion(uid: String) {
-        questionList.remove(uid)
+        questionMap.remove(uid)
+        for (question in questionList)
+            if (question.uid == uid) {
+                questionList.remove(question)
+                break
+            }
     }
 
 }
--- app/src/main/java/com/debugg3r/android/testdecisions/ui/TextItemListAdapter.kt
+++ app/src/main/java/com/debugg3r/android/testdecisions/ui/TextItemListAdapter.kt
@@ -8,8 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
+import android.widget.Toast
 import com.debugg3r.android.testdecisions.R
-import com.debugg3r.android.testdecisions.data.TextItem
+import com.debugg3r.android.testdecisions.data.Question
 import kotlinx.android.synthetic.main.layout_list_item_addnew.view.*
 
 enum class TextItemViewType(value: Int) {
@@ -17,7 +18,7 @@ enum class TextItemViewType(value: Int) {
 }
 
 class TextItemListAdapter: RecyclerView.Adapter<TextItemListAdapter.ViewHolder> {
-    var presenter:QuestionPresenterInterface?
+    var presenter: QuestionPresenterInterface?
 
     constructor(presenter: QuestionPresenterInterface) {
 
@@ -33,21 +34,44 @@ class TextItemListAdapter: RecyclerView.Adapter<TextItemListAdapter.ViewHolder> 
 
         Log.d(LOG_TAG, "list position: ${position} with list size: ${questionCount}")
 
-        var targetVisibility = if (position == presenter?.getQuestionCount()) View.VISIBLE else View.GONE
+        var question: Question? = if (position < questionCount) presenter?.getQuestion(position) else null
+
+        holder.tv_item.setTag(R.integer.tag_holder, holder)
+        holder.tv_item.setTag(R.integer.tag_uid, question?.uid)
+        holder.et_item.setTag(R.integer.tag_holder, holder)
+        holder.et_item.setTag(R.integer.tag_uid, question?.uid)
+        holder.btn_add.setTag(R.integer.tag_holder, holder)
+        holder.btn_add.setTag(R.integer.tag_uid, question?.uid)
+
+        var targetVisibility = if (position == questionCount) View.VISIBLE else View.GONE
         if (holder.btn_add.visibility != targetVisibility)
-                holder.btn_add.visibility = targetVisibility
+            holder.btn_add.visibility = targetVisibility
+
+        if (holder.btn_add.visibility == View.VISIBLE) {
+            holder.btn_add.setOnClickListener { view -> onBtnClick(view) }
+        }
 
         targetVisibility = if (position < questionCount && position != currentEditing) View.VISIBLE else View.GONE
         if (holder.tv_item.visibility != targetVisibility)
             holder.tv_item.visibility = targetVisibility
 
+        if (holder.tv_item.visibility == View.VISIBLE) {
+            holder.tv_item.setOnClickListener { view -> onTvClick(view) }
+            holder.tv_item.setOnLongClickListener { view -> onLongClick(view) }
+        }
+
         targetVisibility = if (position == currentEditing) View.VISIBLE else View.GONE
         if (holder.et_item.visibility != targetVisibility)
             holder.et_item.visibility = targetVisibility
 
+        if (holder.et_item.visibility == View.VISIBLE) {
+            holder.et_item.setOnEditorActionListener { view, actionId, event -> onEditorAction(view, actionId, event) }
+            holder.et_item.setOnFocusChangeListener { view, hasFocus -> onFocusChange(view, hasFocus) }
+        }
+
         if (position < questionCount) {
-            holder.et_item.setText(presenter?.getQuestion(position)?.text)
-            holder.tv_item.setText(presenter?.getQuestion(position)?.text)
+            holder.et_item.setText(question?.text)
+            holder.tv_item.setText(question?.text)
         }
     }
 
@@ -62,32 +86,36 @@ class TextItemListAdapter: RecyclerView.Adapter<TextItemListAdapter.ViewHolder> 
         val layoutInflater = LayoutInflater.from(parent.context)
         val view = layoutInflater.inflate(R.layout.layout_list_item_addnew, parent, false)
 
-        view.et_text_item.setOnEditorActionListener{ view, actionId, event -> onEditorAction(view, actionId, event) }
-        view.tv_text_item.setOnClickListener{ view -> onClick(view) }
-
         return ViewHolder(view)
     }
 
+    private fun onFocusChange(view: View, hasFocus: Boolean) {
+        val holder = view.getTag(R.integer.tag_holder) as ViewHolder
+        if (view.visibility == View.VISIBLE && !hasFocus)
+            //notifyItemChanged(holder.adapterPosition)
+            onBindViewHolder(holder, holder.adapterPosition)
+    }
+
     override fun getItemCount(): Int {
-//        if (presenter == null)
-//            return 1
         return 1 + (presenter?.getQuestionCount() ?: 0)
     }
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val btn_add = itemView.btn_add_text_item
         val tv_item = itemView.tv_text_item
         val et_item = itemView.et_text_item
     }
 
-    fun onEditorAction(view: TextView, actionId: Int, event: KeyEvent): Boolean {
+    fun onEditorAction(view: TextView, actionId: Int, event: KeyEvent?): Boolean {
         if (actionId == EditorInfo.IME_ACTION_DONE) {
             val position: Int? = currentEditing
             currentEditing = null
 
             if (position != null) {
+                Log.d(LOG_TAG, "Item changed in position " + position)
                 val question = presenter?.getQuestion(position)
-                question?.text = view.text as String
+                //val question = presenter?.getQuestion(view.getTag(1) as String)
+                question?.text = view.text.toString()
                 notifyItemChanged(position)
             }
             return true
@@ -95,8 +123,32 @@ class TextItemListAdapter: RecyclerView.Adapter<TextItemListAdapter.ViewHolder> 
         return false
     }
 
-    fun onClick(view: View) {
+    fun onBtnClick(view: View) {
 
+    }
+
+    fun onTvClick(view: View) {
+        dropEditing()
+    }
+
+    fun onLongClick(view: View): Boolean {
+        //dropEditing()
+        //Toast.makeText(, "Long click", Toast.LENGTH_SHORT)
+        val holder = view.getTag(R.integer.tag_holder) as ViewHolder
+        currentEditing = holder.adapterPosition
+        Log.d(LOG_TAG, "Long click performed")
+        if (currentEditing != null)
+            //notifyItemChanged(holder.adapterPosition, 1)
+            onBindViewHolder(holder, holder.adapterPosition)
+        return true
+    }
+
+    fun dropEditing() {
+        if (currentEditing != null) {
+            val curr = currentEditing ?: 0
+            currentEditing = null
+            notifyItemChanged(curr, 1)
+        }
     }
 
 }
