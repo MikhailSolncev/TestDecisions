8517ac0a64f23958d2a6230929b9b1bf18840117
Debugg3R <debugg3r@gmail.com>
2018-12-18 22:44:19 +0300

    Universal adapter for list

---
+++ app/src/main/java/com/debugg3r/android/testdecisions/data/ListDataset.kt
@@ -0,0 +1,9 @@
+package com.debugg3r.android.testdecisions.data
+
+interface ListDataset {
+    fun getCount(): Int
+    fun addElement(): Int
+    fun deleteElement(position: Int): Int
+    fun getElement(position: Int): TextItem
+    fun newItem(text: String): TextItem
+}
